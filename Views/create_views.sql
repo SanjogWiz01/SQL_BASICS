34d13a821f5822bcf9718033c79164291cd0fa72
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-02-06 21:57:12 +0545

    views

---
+++ Views/create_views.sql
@@ -0,0 +1,18 @@
+-- Active: 1768452168398@@127.0.0.1@3306@having_clause_db
+use having_clause_db;
+create table gagets (
+    id int primary key auto_increment,
+    name varchar(50),
+    price decimal(10,2)
+);
+insert into gagets (id,name, price) values
+(1, 'phone', 100.00),
+(2, 'laptop', 150.00),
+(3, 'microphone', 200.00),
+(4, 'headphone', 250.00),
+(5, 'moniter', 300.00);
+select * from gagets;
+create view  MINUTE_MICROSECOND_view as
+select name, price from gagets where price >=150;
+select * from MINUTE_MICROSECOND_view;
+drop view IF EXISTS MINUTE_MICROSECOND_view;
