5792ebbb6a321192c640ef50280e0235852e68f4
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-04-04 21:30:34 +0545

    views

--- Views/create_simple_views.sql
+++ Views/create_simple_views.sql
@@ -4,3 +4,9 @@ FROM table_name
 WHERE condition;
 
 create view sanjog as SELECT age,rollno from students where age < 35 ;
+
+
+CREATE VIEW top_students AS
+SELECT name, grade
+FROM Students
+WHERE marks > 80;
