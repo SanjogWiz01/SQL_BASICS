beae7a267ff347a2a1a21d82695619f7ba23f207
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-04-04 21:32:38 +0545

    create views

--- Views/create_simple_views.sql
+++ Views/create_simple_views.sql
@@ -10,3 +10,9 @@ CREATE VIEW top_students AS
 SELECT name, grade
 FROM Students
 WHERE marks > 80;
+
+
+CREATE VIEW employee_department_info AS
+SELECT e.employee_id, e.first_name, e.last_name, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id;
